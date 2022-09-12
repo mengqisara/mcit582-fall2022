4d2e637188aafb5ed7e558d608d3dad05bda625e
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-09-12 01:26:13 -0700

    Update hash_collision.py

--- hash_collision.py
+++ hash_collision.py
@@ -2,14 +2,6 @@ import hashlib
 import os
 
 def hash_collision(k):
-    if not isinstance(k,int):
-        print( "hash_collision expects an integer" )
-        return( b'\x00',b'\x00' )
-    if k < 0:
-        print( "Specify a positive number of bits" )
-        return( b'\x00',b'\x00' )
-   
-    #Collision finding code goes here
     size=64
     dic={}
     seen=set()
@@ -17,17 +9,17 @@ def hash_collision(k):
     s1=hashlib.sha256(s).hexdigest() #hexdecimal
     sbit=bin( int( s1, 16 ) )[2:]  #binary string
     seen.add(s)
-    dic[sbit[0:k]] = s
+    dic[sbit[len(sbit)-k:len(sbit)]] = s
     while(True):
       x=os.urandom(size)
       if x in seen:
           continue
       x1 = hashlib.sha256(x).hexdigest()
       xbit=bin( int( x1, 16 ) )[2:]
-      if xbit[0:k] in dic:
-          return (x,dic.get(xbit[0:k]))
+      if xbit[len(xbit)-k:len(xbit)] in dic:
+          return (x,dic.get(xbit[len(xbit)-k:len(xbit)]))
       seen.add(x)
-      dic[xbit[0:k]] = x
+      dic[xbit[len(xbit)-k:len(xbit)]] = x
     
 
 
