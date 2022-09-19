5feda9c5e4460bdc07ddd8f102080995c0d4cbf9
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-09-19 02:30:47 -0700

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -14,19 +14,6 @@ def power(g,a,p):
     return res
 
 
-def inverseModular(c1,a,p):
-    if a==1:
-        return pow(c1,-1,p)
-    if a%2==1:
-        return pow(c1,-1,p)*inverseModular(c1,a-1,p)%p
-    d = pow(c1,-1,p)*pow(c1,-1,p)%p
-    a = (int)(a/2)
-    while(a>1):
-        d = d*d%p
-        a = (int)(a/2)
-    return d
-
-
 def keygen():
     sk = random.randrange(1, p)
     pk=power(g,sk,p)
