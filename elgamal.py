e9705f3a786002a63f171f9633a2c0c2681775c8
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-09-19 00:30:35 -0700

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -20,29 +20,27 @@ def inverseModular(c1,a,p):
     if a%2==1:
         return pow(c1,-1,p)*inverseModular(c1,a-1,p)%p
     d = pow(c1,-1,p)*pow(c1,-1,p)%p
-    a=(int)(a/2)
+    a = (int)(a/2)
     while(a>1):
-        d=d*d%p
-        a=(int)(a/2)
+        d = d*d%p
+        a = (int)(a/2)
     return d
-    
 
 
 def keygen():
-    sk = random.randrange(1, p)
+    sk = random.randrange(2, p-1)
     pk=power(g,sk,p)
     return pk,sk
 
 
 def encrypt(pk,m):
-    r=random.randrange(1, p)
+    r=random.randrange(2, p-1)
     c1 = power(g,r,p)
-    c2 = (power(pk,r,p)*power(m,1,p))%p
+    c2 = (power(pk,r,p)*(m%p))%p
     return [c1,c2]
 
 
 def decrypt(sk,c):
     d = inverseModular(c[0],sk,p)
-    m = (c[1]%p*d)%p
+    m = (c[1]*d)%p
     return m
-
