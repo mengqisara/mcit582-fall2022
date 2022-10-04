c67a4b2285dbbf29206276ccc3684cd5e0ecbf1b
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-10-03 18:38:06 -0700

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,27 +1,20 @@
 import requests
 import json
-
-
 def pin_to_ipfs(data):
-    assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
-    # YOUR CODE HERE
-    data=json.dumps(data)
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=json.loads(data),
-                         auth=("2FdXTIiRv8TN32iTleW73bF2fDT", "03cea3e5502b95afa44d342fb3485046"))
-    
+	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+	#YOUR CODE HERE
+	files = {'file': json.dumps(data)}
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=('2AY9kF5JsISRy5RUfzN5dDxebUy','748939e110ae212061018c8310b64792'))
+	cid = response.json()['Hash']
+	return cid
 
-    #hash = p['Hash']
-    return response.text[0:].split(",")[1].split(":")[1].replace('"','')
-
-
-def get_from_ipfs(cid, content_type="json"):
-    assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
-    # YOUR CODE HERE	
-    params = (
-        ('arg', cid),
-    )
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params,
-                             auth=("2FdXTIiRv8TN32iTleW73bF2fDT", "03cea3e5502b95afa44d342fb3485046"))
-    data = json.loads(response.text)
-    assert isinstance(data, dict), f"get_from_ipfs should return a dict"
-    return data
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE	https://ipfs.infura.io:5001/api/v0/cat?arg={content ID}
+	# print(cid)
+	params = (('arg',cid),)
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2AY9kF5JsISRy5RUfzN5dDxebUy','748939e110ae212061018c8310b64792'))
+	data = json.loads(response.text)
+	# print(data)
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
