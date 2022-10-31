fdf9a1c872ac83ac5e2b81b091a6c53c71c6a477
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-10-31 00:51:17 -0700

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -4,7 +4,7 @@ def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	files = {'file': json.dumps(data)}
-	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=('2AY9kF5JsISRy5RUfzN5dDxebUy','748939e110ae212061018c8310b64792'))
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=('2FdXTIiRv8TN32iTleW73bF2fDT','03cea3e5502b95afa44d342fb3485046'))
 	cid = response.json()['Hash']
 	return cid
 
@@ -13,7 +13,7 @@ def get_from_ipfs(cid,content_type="json"):
 	#YOUR CODE HERE	https://ipfs.infura.io:5001/api/v0/cat?arg={content ID}
 	# print(cid)
 	params = (('arg',cid),)
-	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2AY9kF5JsISRy5RUfzN5dDxebUy','748939e110ae212061018c8310b64792'))
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2FdXTIiRv8TN32iTleW73bF2fDT','03cea3e5502b95afa44d342fb3485046'))
 	data = json.loads(response.text)
 	# print(data)
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
