66e30941f23eeafeb51b7123d1d0f30b8bb67176
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-10-31 00:10:47 -0700

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -41,7 +41,7 @@ def get_ape_info(apeID):
 	}
 	try:
 		response = requests.post( 'https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2FdXTIiRv8TN32iTleW73bF2fDT','03cea3e5502b95afa44d342fb3485046'))
-		res1 = json.loads(response.json())
+		res1 = json.loads(json.dumps(response.json()))
 	except Exception as e:
 		print( f"Failed to get {result} from IPFS node" )
 		print( e )
