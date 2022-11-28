eeed6e439972271f6fd19583da8a08de34cef035
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-11-27 20:03:38 -0800

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -3,17 +3,19 @@ from zksk import utils
 import zksk
 import random
 from petlib.bn import Bn
+from petlib.ec import EcGroup
 
 def ZK_equality(G,H):
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
-    r1 = Secret()
-    r2 = Secret()
-    m = Secret()
+    group = EcGroup()
+    r1 = Secret(group.order().random())
+    r2 = Secret(group.order().random())
+    m = Secret(Bn(42))
     C1 = r1.value*G
-    C2 = r1 * H + m * G 
+    C2 = r1.value * H + m.value * G 
     D1 = r2.value*G
-    D2 = r2 * H + m * G
+    D2 = r2.value * H + m.value * G
 
     #Generate a NIZK proving equality of the plaintexts
     stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
