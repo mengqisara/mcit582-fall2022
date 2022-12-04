a20c6679704577bd1aac9621a3d9f6d578d64b4f
mengqisara <62856438+mengqisara@users.noreply.github.com>
2022-12-03 22:03:29 -0800

    Update mint_nft.py

--- mint_nft.py
+++ mint_nft.py
@@ -77,4 +77,4 @@ def mint_nft(nft_contract, tokenId, metadata, owner_address, minter_address):
     cid = pin_to_ipfs(metadata)
 # Step 2:Call "mint" on the contract, set tokenURI to be "ipfs://{CID}" where CID was obtained from step 1
     tokenURI = "ipfs://{cid}"
-    mint(owner_address, tokenId, tokenURI)
+    ERC721.mint(owner_address, tokenId, tokenURI)
